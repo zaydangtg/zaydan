3f969b1828b0d61f9da6f3b043a8b3684f91c7e7
zaydangtg <zaydanrizky4@gmail.com>
2024-08-21 11:43:51 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
+
+mix.js('resources/js/app.js', 'public/js')
+   .postCss('resources/css/app.css', 'public/css', [
+       require('tailwindcss'),
+   ]);
